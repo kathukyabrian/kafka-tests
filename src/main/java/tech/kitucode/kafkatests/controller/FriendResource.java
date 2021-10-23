6aff1d77027dedfab2ebedf55db5331d35f1b434
brian <briankathukya2000@gmail.com>
2021-10-23 10:28:09 +0300

    1. Configured Kafka 2. Write events to Kafka

--- src/main/java/tech/kitucode/kafkatests/controller/FriendResource.java
+++ src/main/java/tech/kitucode/kafkatests/controller/FriendResource.java
@@ -1,5 +1,6 @@
 package tech.kitucode.kafkatests.controller;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 import tech.kitucode.kafkatests.domain.Friend;
 import tech.kitucode.kafkatests.service.FriendService;
@@ -12,19 +13,21 @@ public class FriendResource {
 
     private final FriendService friendService;
 
-    public FriendResource(FriendService friendService) {
+    private final KafkaTemplate<String, Friend> kafkaTemplate;
+
+    public FriendResource(FriendService friendService, KafkaTemplate<String, Friend> kafkaTemplate) {
         this.friendService = friendService;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping("/friend")
     public Friend save(@RequestBody Friend friend){
-
+        kafkaTemplate.send("friend",friend.getEmail(),friend);
         return friendService.add(friend);
     }
 
     @GetMapping("/friend")
     public Map<String,Friend> getAll(){
-
         return friendService.getAll();
     }
 }
